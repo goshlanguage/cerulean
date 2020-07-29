639c820fb5a1d9f80a13bf4eb89852b44e965672
Jesse Millar <jesse.millar@microsoft.com>
2020-07-29 15:35:39 -0500

    Laying down groundwork for the resourceGroups endpoint

---
+++ services/resourceGroups/handlers.go
@@ -0,0 +1,19 @@
+package resourceGroups
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// PutResourceGroupsHandler is the PUT method handler for /subscriptions/{subscription-id}/resourceGroups
+func PutResourceGroupsHandler(subscriptionID string, resourceGroupName string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		b, err := json.Marshal(NewSubscriptionResponse(subscriptionID, resourceGroupName))
+		if err != nil {
+			panic(err)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Charset", "UTF-8")
+		w.Write(b)
+	})
+}
---
+++ services/resourceGroups/models.go
@@ -0,0 +1,38 @@
+package resourceGroups
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+var resourceGroupsJSON = `
+{
+  "id": "/subscriptions/cc6b141e-6afc-4786-9bf6-e3b9a5601460/resourceGroups/DefaultResourceGroup-EUS",
+  "name": "DefaultResourceGroup-EUS",
+  "location": "eastus",
+  "properties": {
+    "provisioningState": "Succeeded"
+  }
+}
+`
+
+// ResourceGroupsResponse models the response from the Azure API when creating or updating a resourceGroup
+type ResourceGroupsResponse struct {
+	ID         string `json:"id"`
+	Name       string `json:"name"`
+	Location   string `json:"location"`
+	Properties struct {
+		ProvisioningState string `json:"provisioningState"`
+	} `json:"properties"`
+}
+
+// NewSubscriptionResponse takes a string ID and returns a basic ResourceGroupsResponse object
+func NewSubscriptionResponse(subscriptionID string, resourceGroupName string) ResourceGroupsResponse {
+	var response ResourceGroupsResponse
+	json.Unmarshal([]byte(resourceGroupsJSON), &response)
+
+	response.ID = fmt.Sprintf("/subscriptions/%s/resourceGroups/%s", subscriptionID, resourceGroupName)
+	response.Name = resourceGroupName
+
+	return response
+}

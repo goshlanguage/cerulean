336d4bc78151d477b756d58752e7368217e4aec2
Ryan Hartje <ryan.hartje@microsoft.com>
2020-07-29 15:08:01 -0500

    adding interface before I forget, need to think about design for this more

---
+++ services/interface.go
@@ -0,0 +1,7 @@
+package services
+
+// TODO Finish service interface and implement
+// Service aims to help autodiscover our avialable services
+type Service interface {
+	GetServiceName() string
+}

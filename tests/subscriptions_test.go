43a6af24b07450a23d9beca094b4319848bb5294
Ryan Hartje <ryan.hartje@microsoft.com>
2020-09-23 09:49:09 -0500

    resolve subscriptions test and list/pagination

--- tests/subscriptions_test.go
+++ tests/subscriptions_test.go
@@ -3,7 +3,6 @@ package tests
 // TODO: Don't use /latest
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2019-11-01/subscriptions"
@@ -12,18 +11,25 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// TestGetSubscriptionsRoute makes a request to the Cerulean API and expects that we get back our default subscriptionID
 func TestGetSubscriptionsRoute(t *testing.T) {
 	server := cerulean.New()
 	client := subscriptions.NewClientWithBaseURI(server.GetBaseClientURI())
 	client.Authorizer = autorest.NullAuthorizer{}
 
-	resultPage, err := client.List(context.TODO())
-	if err != nil {
-		panic(err)
+	var subs []subscriptions.Subscription
+	for subList, err := client.List(context.Background()); subList.NotDone(); err = subList.Next() {
+		if err != nil {
+			t.Errorf("failed to get list of subs: %v", err)
+			t.Fail()
+		}
+		subs = append(subs, subList.Values()...)
 	}
-	// TODO lets render out this object and find out how and what to assert on
-	fmt.Println(resultPage)
-	_, err = resultPage.Values()[0].MarshalJSON()
-	assert.NoErrorf(t, err, "Error raised when marshalling the client response: %s", err)
-	// assert.Containsf(t, json, cerulean.SubscriptionID, "Didn't find our created sub in the azure sdk subscriptions client List resultPage.")
+
+	assert.Equal(
+		t,
+		*subs[0].SubscriptionID,
+		server.SubscriptionID,
+		"Didn't find our created sub in the azure sdk subscriptions.",
+	)
 }

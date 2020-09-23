2eae7786fc5e7fa71442f260a45153ed060e5b43
Jesse Millar <jesse.millar@microsoft.com>
2020-09-23 15:24:11 -0500

    Fixing a panic

--- services/resourceGroups/handlers.go
+++ services/resourceGroups/handlers.go
@@ -1,7 +1,6 @@
 package resourceGroups
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -11,12 +10,6 @@ import (
 func (svc *ResourceGroupsService) PutResourceGroupsHandler() echo.HandlerFunc {
 	// subscriptionID string, resourceGroupName string) http.Handler {
 	return func(c echo.Context) error {
-		b, err := json.Marshal(NewResourceGroupsResponse("subscriptionID", "resourceGroupName"))
-		if err != nil {
-			panic(err)
-		}
-		c.Response().Header().Set("Content-Type", "application/json")
-		c.Response().Header().Set("Charset", "UTF-8")
-		return c.JSON(http.StatusOK, b)
+		return c.JSON(http.StatusOK, NewResourceGroupsResponse("subscriptionID", "resourceGroupName"))
 	}
 }

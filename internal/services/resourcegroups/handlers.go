b7f047a8a934557f5b9d1378349623eb400583d6
Jesse Millar <jesse.millar@microsoft.com>
2020-10-28 09:57:50 -0500

    Starting to add the data store for resource groups

--- internal/services/resourcegroups/handlers.go
+++ internal/services/resourcegroups/handlers.go
@@ -6,6 +6,14 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// GetHandler is the GET method handler for /subscriptions/{subscription-id}/resourceGroups
+func (svc *Service) GetHandler() echo.HandlerFunc {
+	// subscriptionID string, resourceGroupName string) http.Handler {
+	return func(c echo.Context) error {
+		return c.JSON(http.StatusOK, NewResourceGroupsResponse("subscriptionID", "resourceGroupName"))
+	}
+}
+
 // PutHandler is the PUT method handler for /subscriptions/{subscription-id}/resourceGroups
 func (svc *Service) PutHandler() echo.HandlerFunc {
 	// subscriptionID string, resourceGroupName string) http.Handler {
--- internal/services/resourcegroups/service.go
+++ internal/services/resourcegroups/service.go
@@ -4,22 +4,35 @@ import (
 	"net/http"
 
 	"github.com/goshlanguage/cerulean/internal/services"
+	"github.com/goshlanguage/cerulean/pkg/lightdb"
 )
+
+const serviceKey = "resourcegroups"
 
 // Service satisfies the Service interface, and is used to start and maintain the ResourceGroup Service
 type Service struct {
-	// ResourceGroups is our statebag
-	ResourceGroups []ResourceGroup
+	Store *lightdb.Store
 }
 
 // NewService is a factory for Service, which satisfies the services.Service interface
-func NewService() services.Service {
-	return &Service{}
+func NewService(s *lightdb.Store) services.Service {
+	service := &Service{
+		Store: s,
+	}
+
+	return service
 }
 
-// GetHandlers returns the HTTP GET Echo handlers that the service needs in order to operate
+// GetHandlers returns the HTTP Echo handlers that the service needs in order to operate
 func (svc *Service) GetHandlers() map[string]services.Handler {
 	svcMap := make(map[string]services.Handler)
-	svcMap["/subscriptions/:subscriptionId/resourcegroups/:resourceGroupName"] = services.Handler{http.MethodPut, svc.PutHandler()}
+	svcMap["/subscriptions/:subscriptionId/resourcegroups/:resourceGroupName"] = services.Handler{
+		Verb: http.MethodGet,
+		Func: svc.GetHandler(),
+	}
+	svcMap["/subscriptions/:subscriptionId/resourcegroups/:resourceGroupName"] = services.Handler{
+		Verb: http.MethodPut,
+		Func: svc.PutHandler(),
+	}
 	return svcMap
 }

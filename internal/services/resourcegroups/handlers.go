2f8e49284a6347bb12d26d5d16e49c455304dbb0
Jesse Millar <jesse.millar@microsoft.com>
2020-10-28 14:31:36 -0500

    Starting work on a unit test

--- internal/services/resourcegroups/handlers.go
+++ internal/services/resourcegroups/handlers.go
@@ -26,6 +26,7 @@ func (svc *Service) getResourceGroup(c echo.Context) error {
 // PutHandler is the PUT method handler for /subscriptions/{subscription-id}/resourceGroup/{resource-group-name}
 func (svc *Service) PutHandler() echo.HandlerFunc {
 	return func(c echo.Context) error {
+		// TODO: Check if the resource group already exists in the store; return 201 if created and 200 if exists
 		err := svc.AddResourceGroup(c.Param("subscriptionID"), c.Param("resourceGroupName"))
 		if err != nil {
 			return c.JSON(http.StatusInternalServerError, models.NewCloudError(http.StatusInternalServerError, err))
--- internal/services/resourcegroups/handlers_test.go
+++ internal/services/resourcegroups/handlers_test.go
@@ -5,22 +5,39 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/goshlanguage/cerulean/pkg/lightdb"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
 
-// TestPutHandler sets up a server and tests the endpoint directly
-func TestPutHandler(t *testing.T) {
+// TestResourceGroupPutAndGet sets up a server and tests the endpoint directly
+func TestResourceGroupPutAndGet(t *testing.T) {
+	subscriptionID := "12345"
+	resourceGroupName := "testResourceGroupName"
+
 	e := echo.New()
-	req := httptest.NewRequest(http.MethodPost, "/subscriptions/:subscriptionId/resourcegroups/:resourceGroupName", nil)
+	req := httptest.NewRequest(http.MethodPost, "/subscriptions/"+subscriptionID+"/resourcegroups/"+resourceGroupName, nil)
 	rec := httptest.NewRecorder()
 	ctx := e.NewContext(req, rec)
 
-	resourceGroupsService := Service{}
+	s := lightdb.NewStore()
+	resourceGroupsService := Service{
+		Store: s,
+	}
+	putHandler := resourceGroupsService.PutHandler()
 	getHandler := resourceGroupsService.PutHandler()
 
-	// Assertions
+	// Assert that there were no errors and our resource groups service created and returned the new resource group
+	if assert.NoError(t, putHandler(ctx)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Contains(t, rec.Body.String(), subscriptionID)
+		assert.Contains(t, rec.Body.String(), resourceGroupName)
+	}
+
+	// Assert that there were no errors and our resource groups service returned the requested resource group
 	if assert.NoError(t, getHandler(ctx)) {
-		// assert.Equal(t, http.StatusCreated, rec.Code)
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Contains(t, rec.Body.String(), subscriptionID)
+		assert.Contains(t, rec.Body.String(), resourceGroupName)
 	}
 }
